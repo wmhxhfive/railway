2dd67bdb4c832fbab8a040dda870c78ce5a37b13
wmhxhfive <wmhxhfive@126.com>
2018-05-02 10:40:37 +0800

    版本兼容性问题解决--wangming

--- src/main/java/com/railway/wm/WmApplication.java
+++ src/main/java/com/railway/wm/WmApplication.java
@@ -1,9 +1,7 @@
 package com.railway.wm;
 
-import com.railway.wm.security.SecurityInterceptor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
@@ -11,15 +9,6 @@ public class WmApplication extends WebMvcConfigurerAdapter {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WmApplication.class, args);
-	}
 
-	/**
-	 * 配置拦截器
-	 * @author lance
-	 * @param registry
-	 */
-	public void addInterceptors(InterceptorRegistry registry) {
-		registry.addInterceptor(new SecurityInterceptor()).addPathPatterns("/trainInfo/**");
 	}
-
 }
